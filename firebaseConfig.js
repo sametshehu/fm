e013a838440254255e874e99db3861ef09652b25
sametshehu <samet.shehu@me.com>
2024-10-08 21:18:59 +0200

    firebaseConfig.js aktualisieren

---
+++ firebaseConfig.js
@@ -0,0 +1,12 @@
+// firebaseConfig.js
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD5asuJN_0f_wcP3KidUqAYyEsCvtPaIeI",
+  authDomain: "mitgliederverwaltung-3458b.firebaseapp.com",
+  projectId: "mitgliederverwaltung-3458b",
+  storageBucket: "mitgliederverwaltung-3458b.appspot.com",
+  messagingSenderId: "916656812587",
+  appId: "1:916656812587:web:23b510f805c800f62af06e"
+};
+
+export default firebaseConfig;
